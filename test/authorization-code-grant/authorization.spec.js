6bb881d293127d4099ea2a77a4e1d6c400b53ba9
박시율 <kdpark0723@gmail.com>
2019-12-28 13:31:52 +0900

    오류 수정

--- test/authorization-code-grant/authorization.spec.js
+++ test/authorization-code-grant/authorization.spec.js
@@ -1,17 +1,29 @@
 const AuthorizationRequest = require('../../lib/request/authorization-request');
 const responseType = require('../../lib/request/type');
 
+const generatePublicClient = require('../../lib/client/generate/generate-public-client');
+
+const register = require('../client/mock/register-client-data-accessor');
+
 const validate = require('../../lib/authorization-code-grant/authorization/validate');
 
 // eslint-disable-next-line no-undef
 describe('Authorization Code Grant', () => {
   // eslint-disable-next-line no-undef
+  beforeAll(() => {
+    register();
+  });
+
+  // eslint-disable-next-line no-undef
   describe('Authorization Request', () => {
     // eslint-disable-next-line no-undef
-    test('Validation Authorization Request Success', () => {
-      const request = new AuthorizationRequest({ responseType: responseType.CODE, clientId: '' });
+    test('Validation Authorization Request Success', async () => {
+      const client = await generatePublicClient();
+      const request = new AuthorizationRequest({
+        responseType: responseType.CODE, clientId: client.identifier,
+      });
 
-      validate(request);
+      await validate(request);
     });
   });
 });
