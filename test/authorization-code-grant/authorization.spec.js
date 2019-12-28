6b33a1ff061b6b442d0851574ae7c959b74595bf
박시율 <kdpark0723@gmail.com>
2019-12-28 14:23:34 +0900

    오류 수정

--- test/authorization-code-grant/authorization.spec.js
+++ test/authorization-code-grant/authorization.spec.js
@@ -2,10 +2,15 @@ const AuthorizationRequest = require('../../lib/request/authorization-request');
 const responseType = require('../../lib/request/type');
 
 const generatePublicClient = require('../../lib/client/generate/generate-public-client');
-
 const register = require('../client/mock/register-client-data-accessor');
+const authorization = require('../../lib/authorization-code-grant/authorization');
 
-const validate = require('../../lib/authorization-code-grant/authorization/validate');
+const InvalidRequestError = require('../../lib/error/invalid-request');
+const UnsupportedResponseType = require('../../lib/error/unsupported-response-type');
+const UnauthorizedClient = require('../../lib/error/unauthorized-client');
+const InvalidScope = require('../../lib/error/invalid-scope');
+
+const errorPool = require('../../lib/error/pool');
 
 // eslint-disable-next-line no-undef
 describe('Authorization Code Grant', () => {
@@ -17,13 +22,101 @@ describe('Authorization Code Grant', () => {
   // eslint-disable-next-line no-undef
   describe('Authorization Request', () => {
     // eslint-disable-next-line no-undef
-    test('Validation Authorization Request Success', async () => {
+    test('Authorization Request Success', async () => {
       const client = await generatePublicClient();
       const request = new AuthorizationRequest({
         responseType: responseType.CODE, clientId: client.identifier,
       });
 
-      await validate(request);
+      await authorization(request);
+    });
+
+    // eslint-disable-next-line no-undef
+    test('Authorization Request Fail Because responseType is not code', async () => {
+      const client = await generatePublicClient();
+      const request = new AuthorizationRequest({
+        responseType: '', clientId: client.identifier,
+      });
+
+      try {
+        // eslint-disable-next-line no-undef
+        expect.assertions(1);
+        await authorization(request);
+        // eslint-disable-next-line no-undef
+      } catch (e) {
+        // eslint-disable-next-line no-undef
+        expect(e).toEqual(errorPool.get(UnsupportedResponseType));
+      }
+    });
+
+    // eslint-disable-next-line no-undef
+    test('Authorization Request Fail Because responseType is null', async () => {
+      const client = await generatePublicClient();
+      const request = new AuthorizationRequest({
+        responseType: null, clientId: client.identifier,
+      });
+
+      try {
+        // eslint-disable-next-line no-undef
+        expect.assertions(1);
+        await authorization(request);
+        // eslint-disable-next-line no-undef
+      } catch (e) {
+        // eslint-disable-next-line no-undef
+        expect(e).toEqual(errorPool.get(InvalidRequestError));
+      }
+    });
+
+    // eslint-disable-next-line no-undef
+    test('Authorization Request Fail Because clientId is null', async () => {
+      const request = new AuthorizationRequest({
+        responseType: responseType.CODE, clientId: null,
+      });
+
+      try {
+        // eslint-disable-next-line no-undef
+        expect.assertions(1);
+        await authorization(request);
+        // eslint-disable-next-line no-undef
+      } catch (e) {
+        // eslint-disable-next-line no-undef
+        expect(e).toEqual(errorPool.get(InvalidRequestError));
+      }
+    });
+
+    // eslint-disable-next-line no-undef
+    test('Authorization Request Fail Because responseType is not code', async () => {
+      const request = new AuthorizationRequest({
+        responseType: responseType.CODE, clientId: '',
+      });
+
+      try {
+        // eslint-disable-next-line no-undef
+        expect.assertions(1);
+        await authorization(request);
+        // eslint-disable-next-line no-undef
+      } catch (e) {
+        // eslint-disable-next-line no-undef
+        expect(e).toEqual(errorPool.get(UnauthorizedClient));
+      }
+    });
+
+    // eslint-disable-next-line no-undef
+    test('Authorization Request Fail Because scope is not supported', async () => {
+      const client = await generatePublicClient();
+      const request = new AuthorizationRequest({
+        responseType: responseType.CODE, clientId: client.identifier, scope: ['other'],
+      });
+
+      try {
+        // eslint-disable-next-line no-undef
+        expect.assertions(1);
+        await authorization(request);
+        // eslint-disable-next-line no-undef
+      } catch (e) {
+        // eslint-disable-next-line no-undef
+        expect(e).toEqual(errorPool.get(InvalidScope));
+      }
     });
   });
 });
