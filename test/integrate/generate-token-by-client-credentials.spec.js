056ff3474cfeeb3a36b5751e8dd3356e50fa30ce
박시율 <kdpark0723@gmail.com>
2020-01-03 00:05:24 +0900

    리팩토링

--- test/integrate/generate-token-by-client-credentials.spec.js
+++ test/integrate/generate-token-by-client-credentials.spec.js
@@ -9,7 +9,7 @@ const requestMethod = require('../../lib/http/request/request-method');
 const grantType = require('../../lib/token/grant-type');
 
 describe('Generate Token By Client Credentials', () => {
-  test('Generate Token Success By Client Credentials With Client Authorization', async () => {
+  test('Generate Token Success With Client Authorization', async () => {
     const clientDataAccessor = new ClientDataAccessor();
 
     const client = await clientDataAccessor.insert(new Client({
@@ -36,7 +36,7 @@ describe('Generate Token By Client Credentials', () => {
     expect(response.body.expires_in).toBeTruthy();
   });
 
-  test('Generate Token Success By Client Credentials With Client Secret', async () => {
+  test('Generate Token Success With Client Secret', async () => {
     const clientDataAccessor = new ClientDataAccessor();
 
     const client = await clientDataAccessor.insert(new Client({
@@ -60,6 +60,30 @@ describe('Generate Token By Client Credentials', () => {
     expect(response.body.access_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
     expect(response.body.token_type).toEqual('bearer');
     expect(response.body.expires_in).toBeTruthy();
+  });
+
+  test('Generate Token Fail Because Client Not Authorization', async () => {
+    const clientDataAccessor = new ClientDataAccessor();
+
+    const client = await clientDataAccessor.insert(new Client({
+      secret: 'SECRET',
+      scope: ['test'],
+    }));
+
+    const server = createServer(clientDataAccessor);
+
+    const response = await server.token(new Request({
+      method: requestMethod.POST,
+      body: {
+        grant_type: grantType.CLIENT_CREDENTIALS,
+        scope: ['test'],
+        client_id: client.id,
+        client_secret: 'other',
+      },
+    }));
+
+    expect(response.status).toEqual(401);
+    expect(response.body.error).toEqual('unauthorized_client');
   });
 });
 
