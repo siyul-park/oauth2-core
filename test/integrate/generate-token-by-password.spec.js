62cd3edd928d7925a74ea761cf5e284cbe0bc9e3
박시율 <kdpark0723@gmail.com>
2020-01-02 23:45:02 +0900

    리팩토링

--- test/integrate/generate-token-by-password.spec.js
+++ test/integrate/generate-token-by-password.spec.js
@@ -12,7 +12,7 @@ const requestMethod = require('../../lib/http/request/request-method');
 const grantType = require('../../lib/token/grant-type');
 
 describe('Generate Token By Password', () => {
-  test('Generate Token Success By Password', async () => {
+  test('Generate Token Success By Password With Client Authorization', async () => {
     const clientDataAccessor = new ClientDataAccessor();
     const userDataAccessor = new UserDataAccessor();
 
@@ -33,6 +33,70 @@ describe('Generate Token By Password', () => {
       headers: {
         Authorization: `Basic ${client.base64()}`,
       },
+      body: {
+        grant_type: grantType.PASSWORD,
+        username: user.name,
+        password: user.password,
+        scope: ['test'],
+      },
+    }));
+
+    expect(response.status).toEqual(201);
+    expect(response.body.access_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
+    expect(response.body.refresh_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
+    expect(response.body.token_type).toEqual('bearer');
+    expect(response.body.expires_in).toBeTruthy();
+  });
+
+  test('Generate Token Success By Password With Client Secret', async () => {
+    const clientDataAccessor = new ClientDataAccessor();
+    const userDataAccessor = new UserDataAccessor();
+
+    const client = await clientDataAccessor.insert(new Client({
+      secret: 'SECRET',
+      scope: ['test'],
+    }));
+    const user = await userDataAccessor.insert(new User({
+      name: 'test',
+      password: 'test-password',
+      scope: ['test'],
+    }));
+
+    const server = createServer(clientDataAccessor, userDataAccessor);
+
+    const response = await server.token(new Request({
+      method: requestMethod.POST,
+      body: {
+        grant_type: grantType.PASSWORD,
+        username: user.name,
+        password: user.password,
+        scope: ['test'],
+        client_id: client.id,
+        client_secret: client.secret,
+      },
+    }));
+
+    expect(response.status).toEqual(201);
+    expect(response.body.access_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
+    expect(response.body.refresh_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
+    expect(response.body.token_type).toEqual('bearer');
+    expect(response.body.expires_in).toBeTruthy();
+  });
+
+  test('Generate Token Success By Password With Client Secret', async () => {
+    const clientDataAccessor = new ClientDataAccessor();
+    const userDataAccessor = new UserDataAccessor();
+
+    const user = await userDataAccessor.insert(new User({
+      name: 'test',
+      password: 'test-password',
+      scope: ['test'],
+    }));
+
+    const server = createServer(clientDataAccessor, userDataAccessor);
+
+    const response = await server.token(new Request({
+      method: requestMethod.POST,
       body: {
         grant_type: grantType.PASSWORD,
         username: user.name,
