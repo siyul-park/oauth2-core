c70a59e6504546873e13dc9011bd254cb036acea
박시율 <kdpark0723@gmail.com>
2020-01-03 00:16:37 +0900

    리플래쉬 토큰 테스트 추가

--- test/integrate/generate-token-by-reflesh-token.spec.js
+++ test/integrate/generate-token-by-reflesh-token.spec.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 const createServer = require('./create-server');
+const getAuthorizationCode = require('./get-authorization-code');
+const generateTokenByCode = require('./generate-token-by-code');
 
 const ClientDataAccessor = require('../mock/client-data-accessor');
 const Client = require('../../lib/client/client');
-
-const UserDataAccessor = require('../mock/user-data-accessor');
-const User = require('../../lib/resource-owner/user');
 
 const Request = require('../../lib/http/request/request');
 const requestMethod = require('../../lib/http/request/request-method');
@@ -13,20 +12,20 @@ const grantType = require('../../lib/token/grant-type');
 
 describe('Generate Token By Password', () => {
   test('Generate Token Success By Password', async () => {
-    const clientDataAccessor = new ClientDataAccessor();
-    const userDataAccessor = new UserDataAccessor();
+    const redirectUri = 'https://oauth2-core/auth';
 
+    const clientDataAccessor = new ClientDataAccessor();
     const client = await clientDataAccessor.insert(new Client({
       secret: 'SECRET',
       scope: ['test'],
-    }));
-    const user = await userDataAccessor.insert(new User({
-      name: 'test',
-      password: 'test-password',
-      scope: ['test'],
+      redirectUri,
     }));
 
-    const server = createServer(clientDataAccessor, userDataAccessor);
+    const server = createServer(clientDataAccessor);
+    const code = await getAuthorizationCode(server, client, 'test');
+
+    // eslint-disable-next-line max-len
+    const refreshToken = (await generateTokenByCode(server, client, code, redirectUri)).body.refresh_token;
 
     const response = await server.token(new Request({
       method: requestMethod.POST,
@@ -34,18 +33,17 @@ describe('Generate Token By Password', () => {
         Authorization: `Basic ${client.base64()}`,
       },
       body: {
-        grant_type: grantType.PASSWORD,
-        username: user.name,
-        password: user.password,
+        grant_type: grantType.REFRESH_TOKEN,
+        refresh_token: refreshToken,
         scope: ['test'],
       },
     }));
 
     expect(response.status).toEqual(201);
     expect(response.body.access_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
-    expect(response.body.refresh_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
     expect(response.body.token_type).toEqual('bearer');
     expect(response.body.expires_in).toBeTruthy();
+    expect(response.body.scope).toEqual(['test']);
   });
 });
 
