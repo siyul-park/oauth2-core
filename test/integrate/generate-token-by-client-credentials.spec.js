c28ebb28f4c3bab9c39827a579483854844abbe5
박시율 <kdpark0723@gmail.com>
2020-01-02 21:16:31 +0900

    GrantTypeClientTypeCredentials

---
+++ test/integrate/generate-token-by-client-credentials.spec.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+const createServer = require('./create-server');
+
+const ClientDataAccessor = require('../mock/client-data-accessor');
+const Client = require('../../lib/client/client');
+
+const Request = require('../../lib/http/request/request');
+const requestMethod = require('../../lib/http/request/request-method');
+const grantType = require('../../lib/token/grant-type');
+
+describe('Generate Token By Client Credentials', () => {
+  test('Generate Token Success By Client Credentials', async () => {
+    const clientDataAccessor = new ClientDataAccessor();
+
+    const client = await clientDataAccessor.insert(new Client({
+      secret: 'SECRET',
+      scope: ['test'],
+    }));
+
+    const server = createServer(clientDataAccessor);
+
+    const response = await server.token(new Request({
+      method: requestMethod.POST,
+      headers: {
+        Authorization: `Basic ${client.base64()}`,
+      },
+      body: {
+        grant_type: grantType.CLIENT_CREDENTIALS,
+        scope: ['test'],
+      },
+    }));
+
+    expect(response.status).toEqual(201);
+    expect(response.body.access_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
+    expect(response.body.token_type).toEqual('bearer');
+    expect(response.body.expires_in).toBeTruthy();
+  });
+});
+
+/* eslint-enable no-undef */
