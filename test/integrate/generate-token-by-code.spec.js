e8bc0f20a0ad8cf1be44eec84f8bc44f6cbb59f5
박시율 <kdpark0723@gmail.com>
2020-01-02 22:07:00 +0900

    Generate Token By Authorization Code 수정

--- test/integrate/generate-token-by-code.spec.js
+++ test/integrate/generate-token-by-code.spec.js
@@ -11,39 +11,7 @@ const requestMethod = require('../../lib/http/request/request-method');
 const grantType = require('../../lib/token/grant-type');
 
 describe('Generate Token By Authorization Code', () => {
-  test('Generate Token Success By Public Client', async () => {
-    const redirectUri = 'https://oauth2-core/auth';
-
-    const clientDataAccessor = new ClientDataAccessor();
-    const client = await clientDataAccessor.insert(new Client({
-      scope: ['test'],
-      redirectUri,
-    }));
-
-    const server = createServer(clientDataAccessor);
-    const code = await getAuthorizationCode(server, client, 'test');
-
-    const response = await server.token(new Request({
-      method: requestMethod.POST,
-      headers: {
-        Authorization: `Basic ${client.base64()}`,
-      },
-      body: {
-        grant_type: grantType.AUTHORIZATION_CODE,
-        code,
-        redirect_uri: redirectUri,
-        client_id: client.id,
-      },
-    }));
-
-    expect(response.status).toEqual(201);
-    expect(response.body.access_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
-    expect(response.body.refresh_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
-    expect(response.body.token_type).toEqual('bearer');
-    expect(response.body.expires_in).toBeTruthy();
-  });
-
-  test('Generate Token Success By Confidential Client', async () => {
+  test('Generate Token Success By Confidential Client By Authorization', async () => {
     const redirectUri = 'https://oauth2-core/auth';
 
     const clientDataAccessor = new ClientDataAccessor();
@@ -66,6 +34,36 @@ describe('Generate Token By Authorization Code', () => {
         code,
         redirect_uri: redirectUri,
         client_id: client.id,
+      },
+    }));
+
+    expect(response.status).toEqual(201);
+    expect(response.body.access_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
+    expect(response.body.refresh_token).toEqual(expect.stringMatching(/[a-z0-9]+/));
+    expect(response.body.token_type).toEqual('bearer');
+    expect(response.body.expires_in).toBeTruthy();
+  });
+
+  test('Generate Token Success By Confidential Client By secret', async () => {
+    const redirectUri = 'https://oauth2-core/auth';
+
+    const clientDataAccessor = new ClientDataAccessor();
+    const client = await clientDataAccessor.insert(new Client({
+      secret: 'SECRET',
+      scope: ['test'],
+      redirectUri,
+    }));
+
+    const server = createServer(clientDataAccessor);
+    const code = await getAuthorizationCode(server, client, 'test');
+
+    const response = await server.token(new Request({
+      method: requestMethod.POST,
+      body: {
+        grant_type: grantType.AUTHORIZATION_CODE,
+        code,
+        redirect_uri: redirectUri,
+        client_id: client.id,
         client_secret: client.secret,
       },
     }));
@@ -82,6 +80,7 @@ describe('Generate Token By Authorization Code', () => {
 
     const clientDataAccessor = new ClientDataAccessor();
     const client = await clientDataAccessor.insert(new Client({
+      secret: 'SECRET',
       scope: ['test'],
       redirectUri,
     }));
